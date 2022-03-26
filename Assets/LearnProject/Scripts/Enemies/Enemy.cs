6a0285f24a963804c01201f29d7cd0ea855c62e2
Anastasia Kuznetsova <kuzanastyav@gmail.com>
2022-03-26 21:29:25 +0300

    Lesson4

--- Assets/LearnProject/Scripts/Enemies/Enemy.cs
+++ Assets/LearnProject/Scripts/Enemies/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour, ITakeDamage
 {
+    [SerializeField] private bool _isMove = false;
+
     private float _speed = 1f;
     private Vector3 _direction = new Vector3(1, 0, 0);
     private int _stepsLimit = 5;
@@ -26,9 +28,12 @@ public class Enemy : MonoBehaviour, ITakeDamage
 
     private void FixedUpdate()
     {
-        if (Math.Abs((transform.position - _startPosition).magnitude) >= _stepsLimit)
-            _direction = -_direction;
-        Step(Time.fixedDeltaTime);
+        if (_isMove)
+        {
+            if (Math.Abs((transform.position - _startPosition).magnitude) >= _stepsLimit)
+                _direction = -_direction;
+            Step(Time.fixedDeltaTime);
+        }
     }
 
     private void Step(float delta)
---
+++ Assets/LearnProject/Scripts/Subjects/Box.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour, ITakeDamage
+{
+
+    [SerializeField] private int _lifePoint;
+
+    public void TakeDamage(int damage)
+    {
+        _lifePoint -= damage;
+        if (_lifePoint <= 0)
+        {
+            Destroy(gameObject);
+            print("Коробка сломана!");
+        }
+    }
+
+}
